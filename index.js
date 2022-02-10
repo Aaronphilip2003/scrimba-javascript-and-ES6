1f13b083beeb7d0167dd04e56dab9f7f9b6fb9d7
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-02-10 08:08:10 +0530

    used textContent instead of innerText

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ let count=0;
 
 function increment(){
     count=count+1;
-    countEl.innerText=count;
+    countEl.textContent=count;
 
 }
 
 function save(){
-    saveEntry.innerText+=" "+count+" -";
+    saveEntry.textContent+=" "+count+" -";
 }
 save()
-
-

8768af66ea6edc0b0628b553532f86914237da15
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-02-10 07:18:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let count=0
+let countEl=document.getElementById("count-el")
+
+let count=0;
 
 function increment(){
-    console.log("The Button was Clicked!")
+    count=count+1;
+    countEl.innerText=count;
+
 }

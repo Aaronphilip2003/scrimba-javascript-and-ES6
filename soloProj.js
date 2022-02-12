603c5d1cd1e320f04f6214055e026a8249cbdec9
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-02-12 07:08:41 +0530

    updated javascript

--- soloProj.js
+++ soloProj.js
@@ -2,4 +2,10 @@ let mf=document.getElementById("meterfeet")
 let met=20
 let feet=20
 
-mf.textContent=met+" meters = "+ (met*3.28)+" feet";
+mf.textContent=met+" meters = "+ (met*3.28)+" feet"+"           |          "+feet+" feet = "+ (feet*0.304)+" meters";
+
+let water=document.getElementById("litregallons")
+let lit=20
+let gal=20
+
+water.textContent=lit+" litres = "+ddd(lit*0.26)+" gallons"

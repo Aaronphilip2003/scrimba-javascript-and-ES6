606d2104674039cdacdb0e334d6f0f0e5988eb21
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-02-14 17:01:52 +0530

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,42 @@
-let firstCard=11
-let secondCard=10
-let sum=firstCard+secondCard
-let message=""
-
-let hasblackjack=false
-let isAlive=true
-
+let cards=[]
+let sum=0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
+let cardsEl=document.getElementById("cards-el")
 
-function startgame(){
+function getRandomCard(){
+    let randomNumber=Math.floor(Math.random()*13)+1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame(){
+    isAlive=true
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]   
+    sum=firstCard+secondCard 
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent="Cards: "
+
+    for(let i=0;i<cards.length;i++)
+    {
+        cardsEl.textContent+=cards[i]+" "
+    }
+
+        sumEl.textContent = "Sum: " + sum
+
     if (sum<21)
     {
         message="Do you want to draw a new card?"
@@ -25,8 +52,12 @@ function startgame(){
         isAlive=false
     }
     messageEl.textContent=message  
-    sumEl.textContent="Sum:"+sum
+    
 }
 
-
-
+function newCard(){ 
+    let card=getRandomCard()
+    sum+=card
+    cards.push(card)
+    renderGame()
+}
